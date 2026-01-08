f09be0c87725db9b57cac04c82ad4654e8ef88f8
asoMan <aso.mansoury@gmail.com>
2026-01-07 21:14:35 -0500

    AI Token Usage

---
+++ TokenUsage/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using OpenAI.Chat;
+
+Console.WriteLine("Hello, World!");
+
+
+AzureOpenAIClient client = new AzureOpenAIClient(new Uri(""), new System.ClientModel.ApiKeyCredential(""));
+AIAgent agent = client.GetChatClient("sdfsdf").CreateAIAgent("You are a Friendly AI Bot, answering questions");
+
+string question = "What is the capital of France and how many people live there?";
+
+AgentRunResponse response = await agent.RunAsync(question);
+Console.WriteLine($"- Input Tokens : {response.Usage?.InputTokenCount}");
+Console.WriteLine($"- Output Tokens : {response.Usage?.OutputTokenCount}");
+
+List<AgentRunResponseUpdate> updates = new List<AgentRunResponseUpdate>();
+await foreach(AgentRunResponseUpdate update in agent.RunStreamingAsync(question))
+{
+    updates.Add(update);
+    Console.Write(update);
+}
